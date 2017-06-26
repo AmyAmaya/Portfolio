79ffcb81feb232407972ae9246cac3a62cc11a84
AmyAmaya <amyamaya12@gmail.com>
2017-06-26 15:34:28 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,6 +8,21 @@
 onmouseout="style.color='black'">
 aboutme</h1>
 
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "aboutme.";
+}
+</script>
+</head>
+
+<body>
+
+<h2>amyamaya</h2>
+
+<p id="demo">click here.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
 </body>
 </html>
 
