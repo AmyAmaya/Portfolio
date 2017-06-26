9ea989a363962f27c4c9d2609c139c5fdd24a24a
AmyAmaya <amyamaya12@gmail.com>
2017-06-26 15:06:35 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,16 +2,16 @@
 <html>
 <body>
  
-<h1 onmouseover="style.color='powderblue'"
-onmouseout="style.color='black'">
-aboutme</h1> 
-
 <h2>JavaScript in Body</h2>
 
 <p id="demo"></p>
 
 <script>
 document.getElementById("demo").innerHTML = "My First JavaScript";
+ 
+<h1 onmouseover="style.color='powderblue'"
+onmouseout="style.color='black'">
+aboutme</h1> 
 </script>
 
 </body>
